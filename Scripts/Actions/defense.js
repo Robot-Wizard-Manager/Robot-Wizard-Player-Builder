5be90a9fa874ada8f1702f5ef6331539795a2ca8
Robot-Wizard-Manager <148084742+Robot-Wizard-Manager@users.noreply.github.com>
2023-10-16 09:30:24 -0500

    Create defense.js

---
+++ Scripts/Actions/defense.js
@@ -0,0 +1,64 @@
+ function addDefenceActions(){
+    actionList.push(new Action("Defense","WeakDefense",(stats,element)=>{return true},
+    (stats,element)=>{
+        var base = parseInt(25 * getMuliOfTwo(stats.defense,50,getElementValue(element,stats),50));
+        var strong =parseInt( base * 1.5);
+        var weak = parseInt(base * .5);
+        var cost = 25;
+        return `Mana Cost ${cost}\n`+
+        `Base ${base} block\n`+
+        `Against ${getElementStrong(element)} ${strong} block\n`+
+        `Against ${getElementWeak(element)} ${weak} block`
+    }));
+        
+    actionList.push(new Action("Defense","BasicDefense",(stats,element)=>{return true},
+    (stats,element)=>{
+        var base = parseInt(50 * getMuliOfTwo(stats.defense,50,getElementValue(element,stats),50));
+        var strong =parseInt( base * 1.5);
+        var weak = parseInt(base * .5);
+        var cost = 40;
+        return `Mana Cost ${cost}\n`+
+        `Base ${base} block\n`+
+        `Against ${getElementStrong(element)} ${strong} block\n`+
+        `Against ${getElementWeak(element)} ${weak} block`
+    }));
+   
+    actionList.push(new Action("Defense","StrongDefense",(stats,element)=>{ if(getElementValue(element,stats) >= 1 || stats.defense >= 2) {return true}; return false; },
+    (stats,element)=>{
+        var base = parseInt(100 * getMuliOfTwo(stats.defense,50,getElementValue(element,stats),50));
+        var strong =parseInt( base * 1.5);
+        var weak = parseInt(base * .5);
+        var cost = 80;
+        return `Mana Cost ${cost}\n`+
+        `Base ${base} block\n`+
+        `Against ${getElementStrong(element)} ${strong} block\n`+
+        `Against ${getElementWeak(element)} ${weak} block`
+        }));
+    actionList.push(new Action("Defense","LuckyDefense",(stats,element)=>{ if((getElementValue(element,stats) >= 1 || stats.defense >= 2) && getElementValue(getElementStrong(element),stats) >= 1) {return true}; return false; },
+    (stats,element)=>{
+        var base = parseInt(50 * getMuliOfTwo(stats.defense,50,getElementValue(element,stats),50));
+        var strong =parseInt( base * 2);
+        var weak = parseInt(base * .5);
+        var strongElement = getElementStrong(element);
+        var cost = 50;
+        return `Has more defense agaist ${strongElement}\n` +
+        `Mana Cost ${cost}\n`+
+        `Base ${base} block\n`+
+        `Against ${strongElement} ${strong} block\n`+
+        `Against ${getElementWeak(element)} ${weak} block`
+        }));
+    actionList.push(new Action("Defense","SafeDefense",(stats,element)=>{if((getElementValue(element,stats) >= 1 || stats.defense >= 2) && getElementValue(getElementWeak(element),stats) >= 1) {return true}; return false; },
+    (stats,element)=>{
+        var base = parseInt(50 * getMuliOfTwo(stats.defense,50,getElementValue(element,stats),50));
+        var strong =parseInt( base * 1.5);
+        var weak = parseInt(base * .80);
+        var strongElement = getElementStrong(element);
+        var weakElement = getElementWeak(element);
+        var cost = 50;
+        return `Loses less defense agaist ${weakElement}\n` +
+        `Mana Cost ${cost}\n`+
+        `Base ${base} block\n`+
+        `Against ${strongElement} ${strong} block\n`+
+        `Against ${weakElement} ${weak} block`
+        }));
+ }
