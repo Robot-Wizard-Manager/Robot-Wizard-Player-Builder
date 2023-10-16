25e6007a62d23a57bbf61b757271ff3302fcfe40
Robot-Wizard-Manager <148084742+Robot-Wizard-Manager@users.noreply.github.com>
2023-10-16 11:33:52 -0500

    Update attack.js

--- Scripts/Actions/attack.js
+++ Scripts/Actions/attack.js
@@ -1,4 +1,17 @@
  function addAttackActions(){
+   actionList.push(new Action("Weak","BasicAttack",
+    (stats,element)=>{return true},
+    (stats,element)=>{
+        
+        var base = parseInt(25 * getMuliOfTwo(stats.attack,50,getElementValue(element,stats),50));
+        var strong =parseInt( base * 1.5);
+        var weak = parseInt(base * .5);
+        var cost = 50;
+        return `Mana Cost ${cost}\n`+
+        `Base ${base} damage\n`+
+        `Against ${getElementStrong(element)} ${strong} damage\n`+
+        `Against ${getElementWeak(element)} ${weak} damage`}
+    ));
     actionList.push(new Action("Attack","BasicAttack",
     (stats,element)=>{return true},
     (stats,element)=>{
