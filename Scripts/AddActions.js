80759bbe204060d26a6a13c54f7f9a8d8f9c5fef
Robot-Wizard-Manager <148084742+Robot-Wizard-Manager@users.noreply.github.com>
2023-10-16 09:29:05 -0500

    Create AddActions.js

---
+++ Scripts/AddActions.js
@@ -0,0 +1,31 @@
+/*
+var listForActions = document.getElementById("actionList");
+const elementLabel = getLabel("Choose a element","element");
+const elementSelect = getSelect("element",["None","Water","Fire","Air","Earth"],"element");
+elementSelect.setAttribute("onchange","setActions(this)");
+const typeLabel = getLabel("Type","type");
+const typeSelect = getSelect("type",["None","Attack","Defense","Special"],"type");
+typeSelect.setAttribute("onchange","setActions(this)");
+const actionLabel = getLabel("Action: ","action");
+const actionSelect = getSelect("action",["None","test"],"action");
+*/
+var listForActions = document.getElementById("actionList");
+var list = document.getElementsByClassName('FullAction')[0];
+
+
+//addAction();
+function addAction(){
+
+    try{
+        listForActions.appendChild(list.cloneNode(true));
+    }catch(error){
+        document.getElementById("test").innerHTML = error;
+       
+    }
+    
+  
+}
+
+
+
+
