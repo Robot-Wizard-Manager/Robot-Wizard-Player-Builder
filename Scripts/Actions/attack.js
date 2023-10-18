929fbb288cb8d70717a75c09af8bb1c6c6e74e43
Robot-Wizard-Manager <148084742+Robot-Wizard-Manager@users.noreply.github.com>
2023-10-18 12:16:00 -0500

    Update attack.js

--- Scripts/Actions/attack.js
+++ Scripts/Actions/attack.js
@@ -36,4 +36,30 @@
         `Against ${getElementStrong(element)} ${strong} damage\n`+
         `Against ${getElementWeak(element)} ${weak} damage`
     }));
+   actionList.push(new Action("Attack","PrecisionAttack",
+    (stats,element)=>{ if((getElementValue(element,stats) >= 1 || stats.attack >= 2) && getElementValue(getElementStrong(element),stats) >= 1) {return true}; return false; },
+    (stats,element)=>{
+        
+        var base = parseInt(50 + (stats.attack * 5) + (getElementValue(element,stats) * 5) );
+        var strong =parseInt( base * 3);
+        var weak = parseInt(base * .5);
+        var cost = 100;
+        return `Mana Cost ${cost}\n`+
+        `Base ${base} damage\n`+
+        `Against ${getElementStrong(element)} ${strong} damage\n`+
+        `Against ${getElementWeak(element)} ${weak} damage`}
+    ));
+  actionList.push(new Action("Attack","LuckyAttack",
+    (stats,element)=>{if((getElementValue(element,stats) >= 1 || stats.attack >= 2) && getElementValue(getElementWeak(element),stats) >= 1) {return true}; return false;},
+    (stats,element)=>{
+        
+        var base = parseInt(50 + (stats.attack * 5) + (getElementValue(element,stats) * 5) );
+        var strong =parseInt( base * 1.5);
+        var weak = parseInt(base * .9);
+        var cost = 100;
+        return `Mana Cost ${cost}\n`+
+        `Base ${base} damage\n`+
+        `Against ${getElementStrong(element)} ${strong} damage\n`+
+        `Against ${getElementWeak(element)} ${weak} damage`}
+    ));
  }
