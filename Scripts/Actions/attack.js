dc23c6b14528db4d5dae297e4a5f4d23c473fc95
Robot-Wizard-Manager <148084742+Robot-Wizard-Manager@users.noreply.github.com>
2023-10-18 11:29:55 -0500

    Update attack.js

--- Scripts/Actions/attack.js
+++ Scripts/Actions/attack.js
@@ -3,7 +3,7 @@
     (stats,element)=>{return true},
     (stats,element)=>{
         
-        var base = parseInt(25 * getMuliOfTwo(stats.attack,50,getElementValue(element,stats),50));
+        var base = parseInt(25 + (stats.attack * 2.5) + (getElementValue(element,stats) * 2.5) );
         var strong =parseInt( base * 1.5);
         var weak = parseInt(base * .5);
         var cost = 50;
@@ -16,7 +16,7 @@
     (stats,element)=>{return true},
     (stats,element)=>{
         
-        var base = parseInt(50 * getMuliOfTwo(stats.attack,50,getElementValue(element,stats),50));
+        var base = parseInt(50 + (stats.attack * 5) + (getElementValue(element,stats) * 5) );
         var strong =parseInt( base * 1.5);
         var weak = parseInt(base * .5);
         var cost = 100;
@@ -27,7 +27,7 @@
     ));
     actionList.push(new Action("Attack","StrongAttack",(stats,element)=>{ if(getElementValue(element,stats) >= 1 || stats.attack >= 2) {return true}; return false; },
     (stats,element)=>{
-        var base = parseInt(100 * getMuliOfTwo(stats.attack,50,getElementValue(element,stats),50));
+        var base = parseInt(100 + (stats.attack * 10) + (getElementValue(element,stats) * 10) );
         var strong =parseInt( base * 1.5);
         var weak = parseInt(base * .5);
         var cost = 210;
