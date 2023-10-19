2606645b3785ec4f4a43e49087d9d440dc49ff98
Robot-Wizard-Manager <148084742+Robot-Wizard-Manager@users.noreply.github.com>
2023-10-19 10:06:58 -0500

    Update BuildActions.js

--- Scripts/BuildActions.js
+++ Scripts/BuildActions.js
@@ -27,14 +27,18 @@
       {return true}; return false;},
     (stats,element)=>{
         
-        var base = parseInt(25 + (stats.attack * 2.5) + (getElementValue(element,stats) * 2.5) );
-        var strong =parseInt( base * 1.5);
-        var weak = parseInt(base * .5);
-        var cost = 50;
-        return `Mana Cost ${cost}\n`+
-        `Base ${base} damage\n`+
-        `Against ${getElementStrong(element)} ${strong} damage\n`+
-        `Against ${getElementWeak(element)} ${weak} damage`}
+        var attack = parseInt(50 + 5 * stats.attck + 5 * getElementValue(element,stats));
+        var block = parseInt(100 + 10 * stats.attck + 10 * getElementValue(element,stats));
+        var strongB =parseInt(block * 1.5);
+        var weakB = parseInt(block * .5);
+        var strongA = parseInt(attack * 1.5);
+        var weakA = parseInt(attack * 1.5);
+        var cost = 125;
+         return "If oppentent does damage then block else attack"+
+        `Mana Cost ${cost}\n`+
+        `Base ${block} block or ${attack} attack\n`+
+        `Against ${getElementStrong(element)} ${strongB} block or ${strongA} attack\n`+
+        `Against ${getElementWeak(element)} ${weakB} block or ${weakA} attack`}
     ));
     
 
