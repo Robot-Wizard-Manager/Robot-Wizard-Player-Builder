8fed4d6035a76afef53b91fb7a9ceffa8ee79ee5
Robot-Wizard-Manager <148084742+Robot-Wizard-Manager@users.noreply.github.com>
2023-10-19 09:35:35 -0500

    Update BuildActions.js

--- Scripts/BuildActions.js
+++ Scripts/BuildActions.js
@@ -1,24 +1,26 @@
   function buildList(){
     addAttackActions();
     addDefenceActions();
-  /*
+  
     actionList.push(new Action("Special","BasicSmart",(stats,element)=>{
          if(getElementValue(element,stats) >= 1 && stats.defense >= 1 && stats.attack >= 1 && stats.special >= 1)
         {return true}; return false; 
     },
     (stats,element)=>{
-        var block = parseInt(50 + 5 * stats.attck + 5 * getElementValue(element,stats);
-        var attack = parseInt(100 + 10 * stats.attck + 10 * getElementValue(element,stats);
-        var strong =parseInt(block * 1.5);
-        var weak = parseInt(attack * .5);
+        var attack = parseInt(50 + 5 * stats.attck + 5 * getElementValue(element,stats);
+        var block = parseInt(100 + 10 * stats.attck + 10 * getElementValue(element,stats);
+        var strongB =parseInt(block * 1.5);
+        var weakB = parseInt(block * .5);
+        var strongA = parseInt(attack * 1.5);
+        var weakA = parseInt(attack * 1.5);
         var cost = 125;
         return "If oppentent does damage then block else attack"+
         `Mana Cost ${cost}\n`+
-        `Base ${base} block/attack\n`+
-        `Against ${getElementStrong(element)} ${strong} block/attack\n`+
-        `Against ${getElementWeak(element)} ${weak} block/attack`
+        `Base ${block} block or ${attack} attack\n`+
+        `Against ${getElementStrong(element)} ${strongB} block or ${strongA} attack\n`+
+        `Against ${getElementWeak(element)} ${weakB} block or ${weakA} attack`
         }));
-    */
+    
 
     
  }
